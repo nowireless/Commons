6e0e0359ac0f221b92304fe486634e0742fb0c67
Ryan Sjostrand <nowirelss@gmail.com>
2015-11-25 23:54:47 -0600

    Some checking is a shutdown hook has been registered.

--- src/main/java/org/nowireless/common/util/ShutDownHookAbstract.java
+++ src/main/java/org/nowireless/common/util/ShutDownHookAbstract.java
@@ -2,14 +2,20 @@ package org.nowireless.common.util;
 
 public abstract class ShutDownHookAbstract extends Thread implements ShutDownHook {
 
+	protected volatile boolean registered = false;
+
 	@Override
 	public void register() {
+		if(this.registered) return;
 		Runtime.getRuntime().addShutdownHook(this);
+		this.registered = true;
 	}
 
 	@Override
 	public void unRegister() {
+		if (!this.registered) return;
 		Runtime.getRuntime().removeShutdownHook(this);
+		this.registered = false;
 	}
 
 	@Override
