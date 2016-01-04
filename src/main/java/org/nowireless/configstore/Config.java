ff0ec91e011116710aa4d2780af61f54f367a605
Ryan Sjostrand <Ryan Sjostrand>
2016-01-04 10:07:55 -0600

    Can lock and unlock a config for multi threading use.

--- src/main/java/org/nowireless/configstore/Config.java
+++ src/main/java/org/nowireless/configstore/Config.java
@@ -1,5 +1,7 @@
 package org.nowireless.configstore;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 import org.nowireless.mstore.store.Entity;
 
 /**
@@ -9,4 +11,17 @@ import org.nowireless.mstore.store.Entity;
  */
 public abstract class Config<T extends Config<T>> extends Entity<T> {
 	
+	private transient final ReentrantLock lock = new ReentrantLock();
+	
+	public void lockConfig() {
+		lock.lock();
+	}
+	
+	public void unlockConfig() {
+		lock.unlock();
+	}
+	
+	public boolean isLocked() {
+		return lock.isLocked();
+	}
 }
